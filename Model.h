e5cf1c1a725979df89e64b05b733fffa9652a5ae
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-11-01 11:55:20 +0900

    feat:マテリアルファイルからライティング情報とテクスチャファイル名を取り出すため、様々な処理追加

---
+++ Model.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <DirectXMath.h>
+#include <d3dx12.h>
+
+class Model
+{
+
+private:
+
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+
+public:
+	struct VertexPosNormalUv
+	{
+		XMFLOAT3 pos; // xyz座標
+		XMFLOAT3 normal; // 法線ベクトル
+		XMFLOAT2 uv;  // uv座標
+	};
+	struct Material
+	{
+		std::string name; //マテリアル名
+		XMFLOAT3 ambient; //アンビエント影響度
+		XMFLOAT3 diffuse; //ディフューズ影響度
+		XMFLOAT3 specular;//スペキュラー影響度
+		float alpha; //アルファ
+		std::string textureFilename; //テクスチャファイル名
+		//コンストラクタ
+		Material() {
+			ambient = { 0.3f,0.3f,0.3f };
+			diffuse = { 0.0f,0.0f,0.0f };
+			specular = { 0.0f,0.0f,0.0f };
+			alpha = 1.0f;
+		}
+	};
+private:
+	// 頂点バッファ
+	ComPtr<ID3D12Resource> vertBuff;
+	// インデックスバッファ
+	ComPtr<ID3D12Resource> indexBuff;
+	// 頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vbView;
+	// インデックスバッファビュー
+	D3D12_INDEX_BUFFER_VIEW ibView;
+	// 頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+	// 頂点インデックス配列
+	
+	std::vector<unsigned short> indices;
+
+	Material material;
+};
+
